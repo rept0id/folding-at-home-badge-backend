9e664e348971f39d765c853fb9d57a1c984fd4d1
rept0id <rad@simplecode.gr>
2024-09-25 00:42:23 +0300

    expiration

--- src/api/badge/views.py
+++ src/api/badge/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 import requests
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
+from datetime import datetime, timedelta
+from email.utils import format_datetime
 
 @api_view(['GET'])
 def badge_view(request, username):
-    # Fetch user data from Folding@home API
     response = requests.get(f'https://api.foldingathome.org/user/{username}/stats')
     if response.status_code != 200:
         return HttpResponse(status=500)
@@ -13,13 +14,18 @@ def badge_view(request, username):
     data = response.json()
     points = data.get('earned', 0)
 
-    # Generate badge URL
     badge_url = f'https://img.shields.io/badge/Folding%40home-{points}-blue'
 
-    # Fetch the badge image
     badge_image = requests.get(badge_url)
     if badge_image.status_code != 200:
         return HttpResponse(status=500)
 
-    # Return the badge image
-    return HttpResponse(badge_image.content, content_type='image/svg+xml')
+    res = HttpResponse(badge_image.content, content_type='image/svg+xml')
+
+    res['Expires'] = format_datetime(datetime.utcnow() + timedelta(hours=12), usegmt=True)
+    res['Cache-Control'] = 'public, max-age=43200'  # 43200 seconds = 12 hours
+    res['Pragma'] = 'no-cache'
+    res['Last-Modified'] = format_datetime(datetime.utcnow(), usegmt=True)
+    res['ETag'] = f'W/"{points}-{int(datetime.utcnow().timestamp())}"'
+
+    return res
