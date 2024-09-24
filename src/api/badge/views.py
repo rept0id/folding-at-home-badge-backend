5041788ec31c9918d59780a53d9a471585d916a8
rept0id <rad@simplecode.gr>
2024-09-25 00:54:44 +0300

    expires

--- src/api/badge/views.py
+++ src/api/badge/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 import requests
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from email.utils import format_datetime
 
 @api_view(['GET'])
@@ -22,7 +22,8 @@ def badge_view(request, username):
 
     res = HttpResponse(badge_image.content, content_type='image/svg+xml')
 
-    res['Expires'] = format_datetime(datetime.now(timezone.utc) + timedelta(hours=12), usegmt=True)    res['Cache-Control'] = 'public, max-age=43200'
+    res['Expires'] = format_datetime(datetime.now(timezone.utc) + timedelta(hours=12), usegmt=True)
+    res['Cache-Control'] = 'public, max-age=43200'
     res['Pragma'] = 'no-cache'
     res['Last-Modified'] = format_datetime(datetime.utcnow(), usegmt=True)
     res['ETag'] = f'W/"{points}-{int(datetime.utcnow().timestamp())}"'
