67198add6a319b8e66a23ec61897bde5d7582224
rept0id <rad@simplecode.gr>
2024-09-25 00:51:17 +0300

    expires

--- src/api/badge/views.py
+++ src/api/badge/views.py
@@ -23,7 +23,7 @@ def badge_view(request, username):
     res = HttpResponse(badge_image.content, content_type='image/svg+xml')
 
     res['Expires'] = format_datetime(datetime.utcnow() + timedelta(hours=12), usegmt=True)    
-    res['Cache-Control'] = 'public, max-age=43200'  # 43200 seconds = 12 hours
+    res['Cache-Control'] = 'public, max-age=43200'
     res['Pragma'] = 'no-cache'
     res['Last-Modified'] = format_datetime(datetime.utcnow(), usegmt=True)
     res['ETag'] = f'W/"{points}-{int(datetime.utcnow().timestamp())}"'
